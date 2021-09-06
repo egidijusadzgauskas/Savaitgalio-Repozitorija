78a1ecba948c1f168b5d84dd77bd56031332c99c
Egidijus Adzgauskas <eadzgauskas@gmail.com>
2021-09-06 18:40:35 +0300

    kas vyksta
    *sukurtas projektas
    *pridetas visual studio failas

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+namespace gitHub_mokymai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var strings = new string[26];
+            for (var i = 0; i < 26; i++)
+                strings[i] = $"\"{(char)('a' + i)}\"";
+            Console.WriteLine($"「{string.Join(",", strings)}」");
+        }
+    }
+}
